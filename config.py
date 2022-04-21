c52a314e9e39a8f5894b62142b258d7cb3bd4382
Lewis ALLEDJI <lewislewis996@gmail.com>
2022-04-21 01:14:41 +0100

    feat: Create Flask-Mail instance

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ app.config['MAIL_USERNAME'] = 'your_mail@gmail.com'
 app.config['MAIL_PASSWORD'] = 'your_email_password'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
