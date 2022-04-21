3a3d83b2eaa561a8caebf4b5e73de6473c161afd
Lewis ALLEDJI <lewislewis996@gmail.com>
2022-04-21 01:23:16 +0100

    add env var to config

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_mail import Mail
-app = Flask(__name__)
+import os
+from dotenv import load_dotenv
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'your_mail@gmail.com'
-app.config['MAIL_PASSWORD'] = 'your_email_password'
+load_dotenv()
+app = Flask(__name__)
+app.config['MAIL_SERVER']=os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
