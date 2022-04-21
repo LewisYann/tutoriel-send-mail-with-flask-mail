9568579dd177b91eec7f5c9d97af0673dc1d7d39
Lewis ALLEDJI <lewislewis996@gmail.com>
2022-04-21 01:14:24 +0100

    feat: Configure your mail provider

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 from flask import Flask
 from flask_mail import Mail
 app = Flask(__name__)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'your_mail@gmail.com'
+app.config['MAIL_PASSWORD'] = 'your_email_password'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
